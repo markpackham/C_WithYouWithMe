d20c137130810b67228214cb868fbf540e2293b9
M <markpackham1@gmail.com>
2024-10-14 13:15:20 +0100

    Create temp calc

---
+++ Temperature/temperature.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+    int choice;
+
+    printf("Temp Convert menu:\n");
+    printf("1. Celsius to Fahrenheit\n");
+    printf("2. Fahrenheit to Celsius\n");
+    printf("Enter your choice (1 or 2) ");
+    scanf("%2", &choice);
+
+    if(choice == 1){
+
+    }else if (choice == 2){
+
+    }else{
+        printf("Invalid choice! please enter 1 or 2!\n");
+    }
+
+    return 0;
+}
