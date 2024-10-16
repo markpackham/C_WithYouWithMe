bfe412393d763858f838fac862053e0932ba7433
M <markpackham1@gmail.com>
2024-10-16 14:06:54 +0100

    Finish off selection sort

--- StringSort/stringSort.cpp
+++ StringSort/stringSort.cpp
@@ -23,7 +23,14 @@ void selectionSort(int array[], int n)
         int minIndex = i;
         for (int j = i + 1; j < n; j++)
         {
+            if(array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
         }
+        int tmp = array[minIndex];
+        array[minIndex] = array[i];
+        array[i] = tmp;
     }
 }
 
@@ -40,6 +47,23 @@ int main()
 {
     int array[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(array) / sizeof(array[0]);
+
+    printf("Original array: \n");
+    printArray(array, n);
+
+    bubbleSort(array, n);
+    printf("Sorted array via Bubble Sort: \n");
+    printArray(array, n);
+
+    int array2[] = {64, 34, 25, 12, 22, 11, 90};
+    selectionSort(array2, n);
+    printf("Sorted array via Selection Sort: \n");
+    printArray(array2, n);
+
+    int array3[] = {64, 34, 25, 12, 22, 11, 90};
+    insertionSort(array3, n);
+    printf("Sorted array via Insertion Sort: \n");
+    printArray(array3, n);
 
     return 0;
 }
