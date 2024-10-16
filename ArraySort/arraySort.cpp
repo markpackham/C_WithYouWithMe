556455f70710c6654cf71187805849d9262ba7cd
M <markpackham1@gmail.com>
2024-10-16 14:15:51 +0100

    Rename as array sort

--- ArraySort/arraySort.cpp
+++ ArraySort/arraySort.cpp
@@ -25,7 +25,7 @@ void selectionSort(int array[], int n)
         int minIndex = i;
         for (int j = i + 1; j < n; j++)
         {
-            if(array[j] < array[minIndex])
+            if (array[j] < array[minIndex])
             {
                 minIndex = j;
             }
@@ -36,10 +36,18 @@ void selectionSort(int array[], int n)
     }
 }
 
-void insertionSort(int array[], int n){
-    for(int i = 1; i < n; i++){
+void insertionSort(int array[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
         int key = array[i];
         int j = i - 1;
+        while (j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
     }
 }
 
