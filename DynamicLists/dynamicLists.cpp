b1a85212db9892ef37e349807c6f625683e1c0db
M <markpackham1@gmail.com>
2024-10-17 12:45:23 +0100

    Add main method for dynamic lists demo

---
+++ DynamicLists/dynamicLists.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *array = NULL;
+    int size = 0;
+    int choice, element, index;
+
+    while (1) {
+        printf("1. Add element\n");
+        printf("2. Remove element\n");
+        printf("3. Update element\n");
+        printf("4. Print elements\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1:
+            printf("Enter an element to add: ");
+        }
+    }
+
+    return 0;
+}
