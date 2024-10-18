6b9ab351b37fcbf16c2cd2d8ae7ad16f87462bc5
M <markpackham1@gmail.com>
2024-10-18 11:39:53 +0100

    Add concat string function

--- PointerString/pointerString.cpp
+++ PointerString/pointerString.cpp
@@ -1,23 +1,38 @@
 #include <stdio.h>
 
-    void reverseString(char *str)
+void reverseString(char *str)
+{
+    char *end = str;
+    while (*end)
     {
-        char *end = str;
-        while (*end)
-        {
-            end++;
-        }
-        end--;
-
-        while (str < end)
-        {
-            char temp = *str;
-            *str = *end;
-            *end = temp;
-            str++;
-            end--;
-        }
+        end++;
     }
+    end--;
+
+    while (str < end)
+    {
+        char temp = *str;
+        *str = *end;
+        *end = temp;
+        str++;
+        end--;
+    }
+}
+
+void concatenateStrings(char *dest, const char *src)
+{
+    while (*dest)
+    {
+        dest++;
+    }
+    while (*src)
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+}
 
 int main()
 {
@@ -27,9 +42,17 @@ int main()
     gets(str1);
     reverseString(str1);
     printf("Reversed string: %s\n", str1);
-    // gets(str2);
-    // concatenateStrings(str1, str2);
-    // printf("Concatenated string: %s\n", str1);
+
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+    concatenateStrings(str1, str2);
+    printf("Concatenated string: %s\n", str1);
+
+    printf("Enter a string to find its length: ");
+    gets(str1);
+    printf("Length of a string: %d\n", stringLength(str1));
 
     return 0;
 }
