ec6abc053433db20aa2299f9f09d107427bd1e67
M <markpackham1@gmail.com>
2024-10-18 11:52:31 +0100

    Demo getting string length

--- PointerString/pointerString.cpp
+++ PointerString/pointerString.cpp
@@ -34,14 +34,29 @@ void concatenateStrings(char *dest, const char *src)
     *dest = '\0';
 }
 
+int stringLength(const char *str)
+{
+    int length = 0;
+    while (*str)
+    {
+        length++;
+        str++;
+    }
+    return length;
+}
+
 int main()
 {
     char str1[100], str2[100];
 
-    printf("Enter a string: ");
+    printf("REVERSE STRING \n");
+
+    printf("Enter a string to reverse: ");
     gets(str1);
     reverseString(str1);
     printf("Reversed string: %s\n", str1);
+
+    printf("\nCOMBINE 1st & 2nd STRING \n");
 
     printf("Enter first string: ");
     gets(str1);
@@ -49,6 +64,8 @@ int main()
     gets(str2);
     concatenateStrings(str1, str2);
     printf("Concatenated string: %s\n", str1);
+
+    printf("\nSTRING LENGTH \n");
 
     printf("Enter a string to find its length: ");
     gets(str1);
