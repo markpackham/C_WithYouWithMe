ae16213e9645672a02b9a4c59b808ea6f7e42ee2
M <markpackham1@gmail.com>
2024-10-21 14:51:30 +0100

    Add Update product function

--- Structs Related/InventoryManagement/inventoryManagement.cpp
+++ Structs Related/InventoryManagement/inventoryManagement.cpp
@@ -15,3 +15,10 @@ void addProduct(Product products[], int *count, char *name, int quantity, float 
     products[*count].price = price;
     (*count)++;
 }
+
+void updateProduct(Product *p, char *name, int quantity, float price)
+{
+    strcpy(p->name, name);
+    p->quantity = quantity;
+    p->price = price;
+}
